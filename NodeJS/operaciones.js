886e6bcbbfa27f6f0b5aeba45304e4570729f3a0
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-12-16 12:49:16 -0400

    Using destructuring to show the output

--- NodeJS/operaciones.js
+++ NodeJS/operaciones.js
@@ -1,6 +1,6 @@
-const ops = require('./demo');
+const { suma, multiplicacion } = require('./demo');
 
-console.log(ops.suma(5, 8));
+// console.log(ops.suma(5, 8));
 
-// console.log(suma(5, 8));
-// console.log(multiplicacion(5, 8));
+console.log(suma(5, 8));
+console.log(multiplicacion(5, 8));
