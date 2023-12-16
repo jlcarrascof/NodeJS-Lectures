682c5a549e25d60ed02bd6414f899611ada47630
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-12-16 12:48:11 -0400

    Using require to import data from demo

--- NodeJS/operaciones.js
+++ NodeJS/operaciones.js
@@ -1,4 +1,6 @@
+const ops = require('./demo');
 
+console.log(ops.suma(5, 8));
 
-console.log(suma(5, 8));
-console.log(multiplicacion(5, 8));
+// console.log(suma(5, 8));
+// console.log(multiplicacion(5, 8));
