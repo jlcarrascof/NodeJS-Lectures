9cf07270f5489a7c966c659cd8ba80b519224e74
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-12-16 23:40:40 -0400

    Apply destructuring

--- NodeJS/operaciones.js
+++ NodeJS/operaciones.js
@@ -1,8 +1,8 @@
 // const { suma, multiplicacion } = require('./demo');
-import ops from './demo.js';
+import {suma, multiplicacion} from './demo.js';
 
-console.log(ops.suma(5, 8));
-console.log(ops.multiplicacion(5, 8));
+console.log(suma(5, 8));
+console.log(multiplicacion(5, 8));
 
 // console.log(suma(5, 8));
 // console.log(multiplicacion(5, 8));
